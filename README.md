7dc1bba51701758301d8239df72b4588b685be15
Oleg Shalygin <oshalygin@gmail.com>
2016-10-17 20:28:09 -0700

    bringing together babel and cleaning up application.js

---
+++ README.md
@@ -0,0 +1,4 @@
+### Google App Engine Flexibile Environment Spike
+
+This is a basic flex app engine NodeJs application
+
--- application.js
+++ application.js
@@ -1,2 +1,24 @@
+/* eslint-disable no-console */
 "use strict";
+import express from "express";
+import colors from "colors"; //eslint-disable-line no-unused-vars
 
+const port = process.env.PORT || 9999; //eslint-disable-line no-process-env
+let application = express();
+
+
+
+application.get("*", (request, response) => {
+    response.status(200).json({
+        name: "gae-flex-spike",
+        version: 1.0,
+        description: "Simple Google App Engine Flexible environment application"
+    });
+});
+
+application.listen(port, (error) => {
+    if (!!error) {
+        console.log(error.bold.red);
+    }
+    console.log(`Application running on http://localhost:${port}`.green);
+});
