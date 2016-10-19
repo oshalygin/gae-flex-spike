9f3884f8766acd40ddb517acc59013a62044fadf
Oleg Shalygin <oshalygin@gmail.com>
2016-10-18 23:26:03 -0700

    stormpath wireup

--- application.js
+++ application.js
@@ -1,10 +1,28 @@
 /* eslint-disable no-console */
 "use strict";
 import express from "express";
+import stormpath from "express-stormpath";
 import colors from "colors"; //eslint-disable-line no-unused-vars
+import {
+    STORMPATH_APPLICATION_HREF,
+    STORMPATH_CLIENT_APIKEY_ID,
+    STORMPATH_CLIENT_APIKEY_SECRET
+} from "/utilities/configurationKeys";
+
 
 const port = process.env.PORT || 9999; //eslint-disable-line no-process-env
 let application = express();
+
+application.use(stormpath.init(application, {
+    apiKey: {
+        id: STORMPATH_CLIENT_APIKEY_ID,
+        secret: STORMPATH_CLIENT_APIKEY_SECRET
+    },
+    application: {
+        href: STORMPATH_APPLICATION_HREF
+    },
+    website: true
+}));
 
 application.get("*", (request, response) => {
     response.status(200).json({
@@ -14,9 +32,13 @@ application.get("*", (request, response) => {
     });
 });
 
-application.listen(port, (error) => {
-    if (!!error) {
-        console.log(error.bold.red);
-    }
-    console.log(`Application running on http://localhost:${port}`.green);
+application.on("stormpath.ready", () => {
+
+    application.listen(port, (error) => {
+        if (!!error) {
+            console.log(error.bold.red);
+        }
+        console.log(`Application running on http://localhost:${port}`.green);
+    });
+
 });
